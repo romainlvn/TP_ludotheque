cc1f2af566f985924506c2c5d9d6df4b1fdf71fb
romain <romain.allavena2025@campus-eni.fr>
2026-02-04 11:59:11 +0100

    Initialize Spring Boot project for TP_ludotheque with Gradle configuration, database connection, and basic structure.

---
+++ src/main/java/fr/eni/tp_ludotheque/TpLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tp_ludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLudothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpLudothequeApplication.class, args);
+    }
+
+}
